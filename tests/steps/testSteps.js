7620cb4378aa5df33ec6d01ebb7caab2c01ec6c8
Natacha Tomkinson <natacha.tomkinson@defra.gov.uk>
2026-05-19 14:02:25 +0100

    chore(#sfd2-991): make BASE_URL configurable

--- tests/steps/testSteps.js
+++ tests/steps/testSteps.js
@@ -3,7 +3,8 @@ import { Given, When, Then } from '@cucumber/cucumber'
 import { expect } from '@playwright/test'
 import { faker } from '@faker-js/faker'
 
-const BASE_URL = 'https://fcp-sfd-frontend.test.cdp-int.defra.cloud'
+const BASE_URL =
+  process.env.BASE_URL || 'https://fcp-sfd-frontend.test.cdp-int.defra.cloud'
 
 Given(
   'I am on SignIn page and enter the credentials for {string}',
